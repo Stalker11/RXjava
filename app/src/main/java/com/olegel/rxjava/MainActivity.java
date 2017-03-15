a6a119bc197731a7195d8228a8aa798f9f95ff49
Oleg Mushenko <oleg.mushenko@teamgear.com.ua>
2017-03-15 17:35:11 +0200

    Added some changes

--- app/src/main/java/com/olegel/rxjava/MainActivity.java
+++ app/src/main/java/com/olegel/rxjava/MainActivity.java
@@ -1,5 +1,12 @@
 package com.olegel.rxjava;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkInfo;
+import android.net.NetworkRequest;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +14,7 @@ import android.widget.TextView;
 
 import com.olegel.rxjava.http.RequestForServer;
 
+import java.lang.reflect.Method;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
@@ -16,43 +24,64 @@ import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableObserver;
 
 public class MainActivity extends AppCompatActivity {
-private TextView textView;
+    private TextView textView;
     private Observer<Long> subscribe;
     private Disposable disp;
     public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.text);
         new RequestForServer().request();
-      disp = Observable.interval(500, TimeUnit.MILLISECONDS)
+        internetConnection();
+        disp = Observable.interval(500, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeWith(new DisposableObserver<Long>() {
                     @Override
                     public void onNext(Long value) {
-                        Log.d(TAG, "onNext:1 "+setTextView());
+                        Log.d(TAG, "onNext:1 " + setTextView());
                     }
 
                     @Override
                     public void onError(Throwable e) {
-                        Log.d(TAG, "onNext:2 "+setTextView());
+                        Log.d(TAG, "onNext:2 " + setTextView());
                     }
 
                     @Override
                     public void onComplete() {
-                        Log.d(TAG, "onNext:3 "+setTextView());
+                        Log.d(TAG, "onNext:3 " + setTextView());
                     }
                 });
 
     }
-    private String setTextView(){
+
+    private String setTextView() {
         textView.setText(Long.toString(System.currentTimeMillis()));
-          return ""; }
+        return "";
+    }
 
     @Override
     protected void onDestroy() {
         disp.dispose();
         super.onDestroy();
+    }
+
+    public void internetConnection() {
+        ConnectivityManager conMan = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+//mobile
+        NetworkInfo.State mobile = conMan.getNetworkInfo(0).getState();
+
+//wifi
+        NetworkInfo.State wifi = conMan.getNetworkInfo(1).getState();
+
+
+        if (mobile == NetworkInfo.State.CONNECTED || mobile == NetworkInfo.State.CONNECTING) {
+            Log.d(TAG, "internetConnection:1 "+mobile);
+        } else if (wifi == NetworkInfo.State.CONNECTED || wifi == NetworkInfo.State.CONNECTING) {
+            Log.d(TAG, "internetConnection:2 "+wifi);
+        }
     }
 }
--- app/src/main/java/com/olegel/rxjava/http/RequestForServer.java
+++ app/src/main/java/com/olegel/rxjava/http/RequestForServer.java
@@ -1,16 +1,18 @@
 package com.olegel.rxjava.http;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import com.olegel.rxjava.models.DescriptionFilm;
 import com.olegel.rxjava.util.ProjectConstants;
 
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import io.reactivex.Observable;
-import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.observers.DisposableObserver;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
@@ -18,7 +20,9 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RequestForServer {
+    private CompositeDisposable mCompositeDisposable;
     private static final String TAG = RequestForServer.class.getSimpleName();
+    private List<DescriptionFilm> mAndroidArrayList;
     private Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(ProjectConstants.HOSTING_URL)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
@@ -44,9 +48,34 @@ public class RequestForServer {
             }
         });
         Observable<ListFilms> filmsObservable = request.getFilmInformationForActor("Quentin%20Tarantino");
-        Observable<DescriptionFilm> desc = filmsObservable.concatMapIterable(ListFilms::equals);
+        mCompositeDisposable = new CompositeDisposable();
+
+        //Observable<DescriptionFilm> desc = filmsObservable.concatMapIterable(this::handleResponse);
+        mCompositeDisposable.add(request.getFilmInformationForActor("Quentin%20Tarantino")
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleResponse,this::handleError));
+
+    }
+
+    private void handleResponse(ListFilms listFilms) {
+        mAndroidArrayList = new ArrayList<DescriptionFilm>((Collection<? extends DescriptionFilm>) listFilms);
+        Log.d(TAG, "handleResponse: "+mAndroidArrayList.get(0).getActors());
+    }
+
+    private void handleResponse(List<DescriptionFilm> androidList) {
+
+
+    }
+
+    private void handleError(Throwable error) {
+
+        Log.d(TAG, "handleError: "+error.getLocalizedMessage());
+    }
+
+
     }
 
 
 
-}
+
